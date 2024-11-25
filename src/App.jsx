8a910b2f32a3d8a02c715db0fe183557daf4d6c6
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-26 09:31:17 +1100

    prank calculator react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2>Prank Calculator</h2>
+    </>
+  );
+}
+
+export default App;
